80a0adc6f14f37a14a0676e3ce2b062aa5f5b806
dat <ngothucdat96@gmail.com>
2022-10-24 09:19:58 +0700

    remove redundant function

--- controllers/product-controllers/service.go
+++ controllers/product-controllers/service.go
@@ -1,9 +1,7 @@
 package product_controllers
 
 import (
-	"fmt"
 	model "github.com/mrdatngo/gin-products/models"
-	"time"
 )
 
 type Service interface {
@@ -27,51 +25,4 @@ func (s *service) SearchProducts(input *InputSearchProducts) ([]*model.EntityPro
 func (s *service) GetProduct(input *InputGetProduct) (*model.EntityProduct, string) {
 	product, errCode := s.repository.GetProductByIdRepository(input)
 	return product, errCode
-}
-
-func (s *service) UpdateEmailService() {
-
-	// query to search engines
-
-	c1 := make(chan string)
-	c2 := make(chan string)
-	c3 := make(chan string)
-
-	go func() {
-		c1 <- SearchGoogle("")
-	}()
-	go func() {
-		c2 <- SearchYahoo("")
-	}()
-	go func() {
-		c3 <- SearchBing("")
-	}()
-
-	result := ""
-	for i := 0; i < 3; i++ {
-		select {
-		case msg1 := <-c1:
-			result += msg1 + "_"
-		case msg2 := <-c2:
-			result += msg2 + "_"
-		case msg3 := <-c3:
-			result += msg3 + "_"
-		}
-	}
-	fmt.Println(result)
-}
-
-func SearchGoogle(msg string) string {
-	time.Sleep(1 * time.Second)
-	return "google"
-}
-
-func SearchYahoo(msg string) string {
-	time.Sleep(1 * time.Second)
-	return "yahoo"
-}
-
-func SearchBing(msg string) string {
-	time.Sleep(2 * time.Second)
-	return "bing"
 }
